ee9be730a33060dc99b61f1b9c7cc0e1c5867444
Agusteando <desarrollo.tecnologico@casitaiedis.edu.mx>
2026-06-10 14:33:04 -0600

    feat: agregar permisos especiales permanentes con recordatorios recurrentes en Telegram

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
+  experimental: { appManifest: false },
   
   modules: [
     '@nuxtjs/tailwindcss'
